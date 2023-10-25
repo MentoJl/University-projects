e4327e865f723e976a61d258a6ff7b4cad50efe8
MentoJl <vladganing32@gmail.com>
2023-10-25 20:32:16 +0300

    task 1 complete

--- Practical work 8/task 1/script.js
+++ Practical work 8/task 1/script.js
@@ -1,3 +1,19 @@
-let clock = document.querySelector("clock")
-let date = Date()
+let clock = document.querySelector(".clock")
+let date = new Date()
 
+let clockGoing = function(clock) {
+    date = new Date()
+    let hours = clock.querySelector(".hours")
+    let minutes = clock.querySelector(".minutes")
+    let seconds = clock.querySelector(".seconds")
+
+    hours.innerText = date.getHours() + ":"
+
+    minutes.innerText = date.getMinutes() < 10 ? "0" + date.getMinutes() + ":" : date.getMinutes() + ":"
+    seconds.innerText = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getUTCSeconds()
+} 
+
+clockGoing(clock)
+setInterval(() => {
+    clockGoing(clock)
+}, 1000)
